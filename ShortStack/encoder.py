c61bc6cd6f9505e02c016747f3a0d4ce93089247
summerela <summerrae78@gmail.com>
2019-04-13 20:44:13 -0700

    added fix for when encoding and s6 have different pool names such as 01 and 1 that was causing some barcodes not to match

--- ShortStack/encoder.py
+++ ShortStack/encoder.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 from numba import jit
 import dask.dataframe as dd
-import multiprocessing as mp
+import more_itertools as mit
 
 pd.options.mode.chained_assignment = None
 
@@ -21,15 +21,15 @@ class Encode_files():
     # instance parameters
     def __init__(self, s6_df, encoding_df, output_dir, prefix, cpus, client):
         self.s6_df = s6_df
-        self.col_names =  [x for x in self.s6_df.columns]
         self.encoding_df = encoding_df
+        self.col_names = [x for x in self.s6_df.columns]
         self.out_dir = output_dir
         self.prefix = prefix
         self.invalids_file = os.path.join(self.out_dir, self.prefix + "_invalids")
         self.cpus = cpus
         self.client = client
     
-    @jit(parallel=True)        
+    @jit(parallel=True)
     def map_basecalls(self, s6_df, encoding_df):
         print("Mapping basecalls.")
         '''
@@ -39,23 +39,27 @@ class Encode_files():
             encoding_df = encoding df containing target and color_code columns
         Output: s6_df with target seqs instead of color codes
         '''
-        
+
+        # make sure that pool id's are the same in both dataframes for merging
+        s6_df['pool'] = s6_df.pool.astype(int)
+        encoding_df['PoolID'] = encoding_df.PoolID.astype(int)
+
         # create join id column
         s6_df["idx"] = s6_df.BC.astype(str) + ":" + s6_df.pool.astype(str)
         encoding_df["idx"] = encoding_df.BC.astype(str) + ":" + encoding_df.PoolID.astype(str)
         
         s6_df["idx"] = s6_df.idx.astype(str)
         encoding_df["idx"] = encoding_df.idx.astype(str)
-        
+
         # match targets to base calls by merging s6_df and encoding_df
         encoded_df = dd.merge(s6_df, encoding_df, 
                               how='left',
                               on='idx')
-        
+
         # drop duplicated columns
         encoded_df = encoded_df.drop(["PoolID", "BC_y", "idx"], axis=1)
         encoded_df = encoded_df.rename(columns={"BC_x":"BC"})
-        
+
         # check for and store info on base calls not valid in encoding file
         parity_df = encoded_df[encoded_df['Target'].isnull()]
         parity_df = parity_df.drop(["Target", "bc_length"], axis=1)
@@ -78,7 +82,7 @@ class Encode_files():
         # remove basecalls not valid in encoding file and reset index
         encoded_df = encoded_df[~encoded_df['Target'].isnull()]
         encoded_df["bc_length"]= encoded_df.bc_length.astype(int)
-        
+
         assert len(encoded_df) != 0, "No valid barcodes found in encoding file."
 
         return encoded_df
